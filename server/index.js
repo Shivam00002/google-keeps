1f9e0e197adae7d5ff5252b0b1b10c63250bf46d
Shivam00002 <dubey02shiv@gmail.com>
2024-08-04 18:23:13 +0530

    fix cors error

--- server/index.js
+++ server/index.js
@@ -8,8 +8,11 @@ const notesRoutes = require("./routes/notes");
 async function StartServer() {
   const app = express();
 
+  // Set CORS options
   const corsOptions = {
-    origin: process.env.FRONTEND_URL || ["http://localhost:3000", "https://dainsta-notes-g4in.vercel.app/"],
+    origin: process.env.FRONTEND_URL 
+      ? process.env.FRONTEND_URL 
+      : ["http://localhost:3000", "https://dainsta-notes-g4in.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   };
@@ -23,6 +26,7 @@ async function StartServer() {
     res.send("Welcome to the API! 🔥");
   });
 
+  // Routes
   app.use("/auth", authRoutes);
   app.use("/notes", notesRoutes);
 
@@ -30,6 +34,7 @@ async function StartServer() {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
+// Start the server
 StartServer().catch((error) => {
-  console.log("error", error);
+  console.error("Error starting the server:", error);
 });
