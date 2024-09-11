7b3fb9e8caaaf8e3e0931163b2e93f70c7dc584e
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-09-11 23:57:40 +0530

    Update AuthContext.tsx

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -12,6 +12,7 @@ interface AuthContextType {
   logout: () => void;
 }
 
+
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({
