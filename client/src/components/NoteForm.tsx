99295a6995b63905745bf7a3f805f2fc4b812d80
Shivam00002 <dubey02shiv@gmail.com>
2024-09-12 15:36:53 +0530

    added text-area

--- client/src/components/NoteForm.tsx
+++ client/src/components/NoteForm.tsx
@@ -72,14 +72,14 @@ const NoteForm: React.FC<NoteFormProps> = ({ onInputChange }) => {
       />
       {showContent && (
         <div className="relative">
-          <input
-            type="text"
-            value={content}
-            name="content"
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="Content"
-            className="w-full md:h-[200px] px-3 py-2 mb-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white"
-          />
+         <textarea
+  value={content}
+  name="content"
+  onChange={(e) => setContent(e.target.value)}
+  placeholder="Content"
+  className="w-full text-left md:h-[200px] px-3 py-2 mb-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white"
+  rows={5}
+/>
 
           <div className="flex border px-2 rounded-md items-center gap-x-3 mb-2">
             <div className="flex items-center space-x-2">
